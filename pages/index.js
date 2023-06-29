0f0dd2532a858ba018e9d2ca7cb8b303cc0ae28e
seolhyelin <tosiltosilkk@gmail.com>
2023-06-29 11:57:54 +0900

    Feat: server side rendering

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,13 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-export default function Home() {
-  const [movies, setMovies] = useState([]);
+export default async function Home() {
+  const results = await fetchData();
 
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch("/api/movies")).json();
-      setMovies(results);
-    })();
-  }, []);
-
-  console.log(movies);
   return (
     <div className="container">
       <Seo title="Home" />
-      <h1>Movies</h1>
-      <br />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -47,4 +36,13 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+export async function fetchData() {
+  const response = await fetch("http://localhost:3000/api/movies", {
+    cache: "no-cache",
+  });
+  const { results } = await response.json();
+
+  return results;
 }
